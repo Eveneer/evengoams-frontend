bc139b9e12967981b457099026e8f16b91201735
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-06 19:38:39 +0600

    Work on transaction line component and add sample transactions

---
+++ src/lib/components/Transactions/Transaction.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import ChevronIcon from '$lib/assets/icons/ChevronIcon.svelte';
+	import type { TransactionType } from '$lib/types/transactions';
+
+	export let transaction: TransactionType;
+
+	const parseDate = (date: string) => {
+		const parts: string[] = date.split('-');
+		const months = [
+			'Jan',
+			'Feb',
+			'Mar',
+			'Apr',
+			'May',
+			'Jun',
+			'Jul',
+			'Aug',
+			'Sep',
+			'Oct',
+			'Nov',
+			'Dec'
+		];
+
+		return `${parts[2]} ${months[parseInt(parts[1])]} ${parts[0]}`;
+	};
+</script>
+
+<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+	<span class="font-mono my-auto">{parseDate(transaction.date)}</span>
+	<span class="flex-1 my-auto line-clamp-1">{transaction.note}</span>
+	<span
+		class="font-mono my-auto {transaction.type === 'income' ? 'text-teal-600' : 'text-orange-600'}"
+		>{transaction.amount}</span
+	>
+	<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+</div>
---
+++ src/routes/transactions/+page.svelte
@@ -0,0 +1,91 @@
+<script lang="ts">
+	import ChevronIcon from '$lib/assets/icons/ChevronIcon.svelte';
+	import Pagination from '$lib/components/Pagination/Pagination.svelte';
+	import Transaction from '$lib/components/Transactions/Transaction.svelte';
+	import transactions from './sample-transactions.json';
+
+	console.log(transactions);
+
+	const paginationData = {
+		total: 50,
+		per_page: 15,
+		current_page: 2,
+		last_page: 4,
+		first_page_url: 'http://laravel.app?page=1',
+		last_page_url: 'http://laravel.app?page=4',
+		next_page_url: 'http://laravel.app?page=2',
+		prev_page_url: 'http://laravel.app?page=1',
+		path: 'http://laravel.app',
+		from: 1,
+		to: 15,
+		data: [
+			{
+				// Record...
+			},
+			{
+				// Record...
+			}
+		]
+	};
+</script>
+
+<div class="flex flex-row gap-5">
+	<div class="w-[60%] flex flex-col gap-5 relative">
+		<div class="sticky top-[20px] bg-slate-300 rounded-2xl">
+			<div class="card !p-3 flex flex-row justify-between">
+				<Pagination {paginationData} />
+				<input type="text" name="" id="" placeholder="Search..." class="w-[40%] search" />
+			</div>
+		</div>
+
+		{#each transactions as transaction}
+			<Transaction {transaction} />
+		{/each}
+		<!-- <div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-orange-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div>
+
+		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-teal-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div>
+
+		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-orange-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div>
+
+		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-teal-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div>
+
+		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-orange-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div>
+
+		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+			<span class="font-mono my-auto">01 Apr 2024</span>
+			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
+			<span class="font-mono my-auto text-teal-600">13,200</span>
+			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
+		</div> -->
+	</div>
+	<div class="w-[40%] relative">
+		<div class="card flex flex-col gap-5 sticky top-[20px]">
+			<h3 class="text-center">Add Transction</h3>
+		</div>
+	</div>
+</div>
