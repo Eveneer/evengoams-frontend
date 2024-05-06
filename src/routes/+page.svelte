e72a8b33c2af8811a7457bd7ee948ee3e99ff49f
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-06 16:28:02 +0600

    Update styling and root +page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="w-full min-h-[85vh] bg-white/50 p-5 rounded-2xl shadow-xl">
+<div class="w-full min-h-[85vh] card">
 	<h1>Welcome to SvelteKit</h1>
 	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 </div>
