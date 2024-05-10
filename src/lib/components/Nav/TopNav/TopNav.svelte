ef5ba8ef7b1e364478f036a42a5f5938e3f9582d
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-10 19:34:26 +0600

    Update transactions layout

--- src/lib/components/Nav/TopNav/TopNav.svelte
+++ src/lib/components/Nav/TopNav/TopNav.svelte
@@ -4,27 +4,31 @@
 	import LogoutIcon from '$lib/assets/icons/LogoutIcon.svelte';
 </script>
 
-<div class="py-2 px-5 bg-white/50 rounded-3xl flex md:flex-row justify-between shadow-xl">
-	<div class="flex flex-row gap-2">
-		<img
-			src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
-			alt="Logo"
-			class="h-[60px]"
-		/>
-		<h3 class="my-auto text-[1.5rem]">Evengoams</h3>
-	</div>
-	<div class="flex flex-row gap-5">
-		<a href="/#" class="flex flex-row gap-2">
-			<SettingsIcon classes="my-auto" size={24} />
-			<span class="my-auto">Configs</span>
-		</a>
-		<a href="/#" class="flex flex-row gap-2">
-			<AuthIcon classes="my-auto" size={24} />
-			<span class="my-auto">Account</span>
-		</a>
-		<a href="/#" class="flex flex-row gap-2">
-			<LogoutIcon classes="my-auto" size={24} />
-			<span class="my-auto">Logout</span>
-		</a>
+<div
+	class="bg-slate-300 rounded-3xl flex md:flex-row justify-between shadow-lg sticky top-[20px] z-10"
+>
+	<div class="bg-white/50 rounded-3xl flex md:flex-row justify-between w-full py-2 px-5">
+		<div class="flex flex-row gap-2">
+			<img
+				src="https://www.adaptivewfs.com/wp-content/uploads/2020/07/logo-placeholder-image.png"
+				alt="Logo"
+				class="h-[60px]"
+			/>
+			<h3 class="my-auto text-[1.5rem]">Evengoams</h3>
+		</div>
+		<div class="flex flex-row gap-5">
+			<a href="/#" class="flex flex-row gap-2">
+				<SettingsIcon classes="my-auto" size={24} />
+				<span class="my-auto">Configs</span>
+			</a>
+			<a href="/#" class="flex flex-row gap-2">
+				<AuthIcon classes="my-auto" size={24} />
+				<span class="my-auto">Account</span>
+			</a>
+			<a href="/#" class="flex flex-row gap-2">
+				<LogoutIcon classes="my-auto" size={24} />
+				<span class="my-auto">Logout</span>
+			</a>
+		</div>
 	</div>
 </div>
--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -3,13 +3,6 @@
 	import Transaction from '$lib/components/Transactions/Transaction.svelte';
 	import t from './sample-transactions.json';
 	import { type TransactionType } from '$lib/types/transactions';
-	import DateInput from '$lib/components/Inputs/DateInput.svelte';
-	import LineInput from '$lib/components/Inputs/LineInput.svelte';
-	import NumberInput from '$lib/components/Inputs/NumberInput.svelte';
-	import RadioInput from '$lib/components/Inputs/RadioInput.svelte';
-	import SearchInput from '$lib/components/Inputs/SearchInput.svelte';
-	import TextInput from '$lib/components/Inputs/TextInput.svelte';
-	import TagsInput from '$lib/components/Inputs/TagsInput.svelte';
 
 	const transactions: TransactionType[] = t as TransactionType[];
 
@@ -34,89 +27,39 @@
 			}
 		]
 	};
-
-	let x: string | number = '';
 </script>
 
 <div class="flex flex-row gap-5">
 	<div class="w-[60%] flex flex-col gap-5 relative">
-		<div class="sticky top-[20px] bg-slate-300 rounded-2xl">
-			<div class="card !p-3 flex flex-row justify-between">
-				<Pagination {paginationData} />
-				<input type="text" name="" id="" placeholder="Search..." class="w-[40%] search" />
-			</div>
-		</div>
-
 		{#each transactions as transaction}
 			<Transaction {transaction} />
 		{/each}
 	</div>
+
 	<div class="w-[40%] relative">
-		<div class="card flex flex-col gap-5 sticky top-[20px]">
-			<h3 class="text-center w-full p-2 bg-white rounded-full">Add Transction</h3>
-			<div class="flex flex-row gap-5">
-				<div class="w-[40%]">
-					<DateInput
-						id="transaction_date"
-						label="Transaction Date"
-						min="2025-05-01"
-						max="2024-06-05"
-						callback={(s) => {}}
-					/>
-				</div>
-				<div class="w-[60%] flex flex-col gap-0">
-					<NumberInput id="transaction_amount" label="Transaction Amount" callback={(s) => {}} />
+		<div class="flex flex-col gap-5 sticky top-[116px]">
+			<div class="card flex flex-col gap-5">
+				<input type="text" name="" id="" placeholder="Search..." class="w-full search" />
+				<div class="mx-auto flex flex-row justify-between w-full">
+					<button class="btn !px-10">Add Transaction</button>
+					<Pagination {paginationData} />
 				</div>
 			</div>
 
-			<div class="flex flex-row gap-5 w-full">
-				<RadioInput
-					label="Transaction Type"
-					value={x}
-					callback={(s) => {
-						x = s;
-					}}
-					options={['Income', 'Expense']}
-				/>
-			</div>
-
-			<div class="flex flex-row gap-5">
-				<div class="w-[50%] flex flex-col gap-0">
-					<SearchInput
-						id="transaction_from"
-						label="Payee"
-						searchCallback={async () => []}
-						callback={(s) => {}}
-					/>
-				</div>
-				<div class="w-[50%] flex flex-col gap-0">
-					<SearchInput
-						id="transaction_to"
-						label="Recipient"
-						searchCallback={async () => []}
-						callback={(s) => {}}
-					/>
-				</div>
-			</div>
-			<div class="flex flex-row gap-5">
-				<TextInput
-					id="transaction_note"
-					label="Transaction Note"
-					placeholder="Note"
-					callback={(s) => {}}
-				/>
-			</div>
-			<div class="flex flex-row gap-5">
-				<TagsInput id="transaction_tags" label="Transaction Tags" callback={(s) => {}} />
-				<!-- 
-                
-                placeholder="Note"
-                callback={(s) => {}} -->
+			<div class="flex flex-col gap- h-[40vh] card w-full justify-center">
+				<h3 class="text-center">Stats Section</h3>
+				<ul class="list-disc mx-auto">
+					<li class="font-mono text-[0.8rem]">
+						Total income in last x period (filterable by x and period)
+					</li>
+					<li class="font-mono text-[0.8rem]">
+						Total expense in last x period (filterable by x and period)
+					</li>
+                    <li class="font-mono text-[0.8rem]">
+                        Linear graph showing incomes and expenses (cash flow)
+                    </li>
+				</ul>
 			</div>
 		</div>
 	</div>
 </div>
-
-<!-- 
-$table->json('tag_ids')->nullable();
--->
