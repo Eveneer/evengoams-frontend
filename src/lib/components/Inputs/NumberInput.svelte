e858faa43f6256f8c980f7052dc053da3f0989c5
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-10 16:40:20 +0600

    Add number input component

---
+++ src/lib/components/Inputs/NumberInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string = id;
+	export let label: string;
+	export let placeholder: string = label;
+	export let min: string = '';
+	export let max: string = '';
+	export let step: string = '';
+	export let callback: (value: number) => void;
+
+	const callCallback = (event: Event) => {
+		callback(Number((event.target as HTMLInputElement).value));
+	};
+</script>
+
+<div class="input-container">
+	<input type="number" {name} {id} {min} {max} {step} {placeholder} on:change={callCallback} />
+	<label for={id}>{label}</label>
+</div>
