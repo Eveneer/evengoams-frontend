0a7411d0e9eff61de2a6c8449aa7ef17c1cfe31b
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-10 16:44:29 +0600

    Work on transactions page

--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
-	import ChevronIcon from '$lib/assets/icons/ChevronIcon.svelte';
 	import Pagination from '$lib/components/Pagination/Pagination.svelte';
 	import Transaction from '$lib/components/Transactions/Transaction.svelte';
-	import transactions from './sample-transactions.json';
+	import t from './sample-transactions.json';
+	import { type TransactionType } from '$lib/types/transactions';
+	import DateInput from '$lib/components/Inputs/DateInput.svelte';
+	import LineInput from '$lib/components/Inputs/LineInput.svelte';
+	import NumberInput from '$lib/components/Inputs/NumberInput.svelte';
+	import RadioInput from '$lib/components/Inputs/RadioInput.svelte';
+	import SearchInput from '$lib/components/Inputs/SearchInput.svelte';
+	import TextInput from '$lib/components/Inputs/TextInput.svelte';
+	import TagsInput from '$lib/components/Inputs/TagsInput.svelte';
 
-	console.log(transactions);
+	const transactions: TransactionType[] = t as TransactionType[];
 
 	const paginationData = {
 		total: 50,
@@ -27,6 +34,8 @@
 			}
 		]
 	};
+
+	let x: string | number = '';
 </script>
 
 <div class="flex flex-row gap-5">
@@ -41,51 +50,73 @@
 		{#each transactions as transaction}
 			<Transaction {transaction} />
 		{/each}
-		<!-- <div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-orange-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div>
-
-		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-teal-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div>
-
-		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-orange-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div>
-
-		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-teal-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div>
-
-		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-orange-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div>
-
-		<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
-			<span class="font-mono my-auto">01 Apr 2024</span>
-			<span class="flex-1 my-auto line-clamp-1">Purpose of the transaction</span>
-			<span class="font-mono my-auto text-teal-600">13,200</span>
-			<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-		</div> -->
 	</div>
 	<div class="w-[40%] relative">
 		<div class="card flex flex-col gap-5 sticky top-[20px]">
-			<h3 class="text-center">Add Transction</h3>
+			<h3 class="text-center w-full p-2 bg-white rounded-full">Add Transction</h3>
+			<div class="flex flex-row gap-5">
+				<div class="w-[40%]">
+					<DateInput
+						id="transaction_date"
+						label="Transaction Date"
+						min="2025-05-01"
+						max="2024-06-05"
+						callback={(s) => {}}
+					/>
+				</div>
+				<div class="w-[60%] flex flex-col gap-0">
+					<NumberInput id="transaction_amount" label="Transaction Amount" callback={(s) => {}} />
+				</div>
+			</div>
+
+			<div class="flex flex-row gap-5 w-full">
+				<RadioInput
+					label="Transaction Type"
+					value={x}
+					callback={(s) => {
+						x = s;
+					}}
+					options={['Income', 'Expense']}
+				/>
+			</div>
+
+			<div class="flex flex-row gap-5">
+				<div class="w-[50%] flex flex-col gap-0">
+					<SearchInput
+						id="transaction_from"
+						label="Payee"
+						searchCallback={async () => []}
+						callback={(s) => {}}
+					/>
+				</div>
+				<div class="w-[50%] flex flex-col gap-0">
+					<SearchInput
+						id="transaction_to"
+						label="Recipient"
+						searchCallback={async () => []}
+						callback={(s) => {}}
+					/>
+				</div>
+			</div>
+			<div class="flex flex-row gap-5">
+				<TextInput
+					id="transaction_note"
+					label="Transaction Note"
+					placeholder="Note"
+					callback={(s) => {}}
+				/>
+			</div>
+			<div class="flex flex-row gap-5">
+				<TagsInput id="transaction_tags" label="Transaction Tags" callback={(s) => {}} />
+				<!-- 
+                
+                placeholder="Note"
+                callback={(s) => {}} -->
+			</div>
 		</div>
 	</div>
 </div>
+
+<!-- 
+$table->json('tag_ids')->nullable();
+-->
