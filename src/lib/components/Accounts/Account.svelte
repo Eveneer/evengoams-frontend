672a68ab48af7f433da65cc526714c89cda6a19f
Mobashir Monim <mobashirmonim@gmail.com>
2024-05-12 21:41:45 +0600

    Make collapsible row component and use it on accounts for row

--- src/lib/components/Accounts/Account.svelte
+++ src/lib/components/Accounts/Account.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
-	import ChevronIcon from '$lib/assets/icons/ChevronIcon.svelte';
 	import type { AccountType } from '$lib/types/accounts';
+	import { tzStampToReadableDateTime } from '$lib/utils';
+	import CollapsibleRow from '../CollapsibleRow/CollapsibleRow.svelte';
 
 	export let account: AccountType;
 </script>
 
-<div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
+<CollapsibleRow>
+	<div slot="row">
+		<span class="font-mono my-auto w-[40px]">{account.type}</span>
+		<span class="flex-1 my-auto line-clamp-1 text-left">{account.name}</span>
+		<span
+			class="font-mono inline-block !mr-auto my-auto {account.balance > 0
+				? 'text-teal-600'
+				: 'text-orange-600'}"
+		>
+			{account.balance}
+		</span>
+	</div>
+
+	<div class="flex flex-col w-full" slot="hidden-content">
+		<span class="text-left">Account Open on: {tzStampToReadableDateTime(account.created_at)}</span>
+	</div>
+</CollapsibleRow>
+<!-- <div class="card flex flex-row justify-between gap-5 text-[0.8rem]">
 	<span class="font-mono my-auto w-[40px]">{account.type}</span>
 	<span class="flex-1 my-auto line-clamp-1">{account.name}</span>
-	<span
-		class="font-mono my-auto {account.balance > 0 ? 'text-teal-600' : 'text-orange-600'}"
+	<span class="font-mono my-auto {account.balance > 0 ? 'text-teal-600' : 'text-orange-600'}"
 		>{account.balance}</span
 	>
 	<span class="my-auto"><ChevronIcon direction="right" size={20} classes="fill-black" /></span>
-</div>
+</div> -->
---
+++ src/lib/components/CollapsibleRow/CollapsibleRow.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import ChevronIcon from '$lib/assets/icons/ChevronIcon.svelte';
+	import autoAnimate from '@formkit/auto-animate';
+
+	let isOpen: boolean = false;
+
+	const toggleOpen = () => (isOpen = !isOpen);
+</script>
+
+<button
+	class="card text-[0.8rem] cursor-pointer flex flex-col"
+	on:click={toggleOpen}
+	use:autoAnimate
+>
+	<div class="collapsible-row-slot">
+		<slot name="row">
+			<h1>Primary Content</h1>
+		</slot>
+		<span class="my-auto"
+			><ChevronIcon direction={isOpen ? 'up' : 'right'} size={20} classes="fill-black" /></span
+		>
+	</div>
+	{#if isOpen}
+		<div class="w-full overflow-hidden max-h-[20vh] mt-3">
+			<div class="border border-gray-500 mb-3"></div>
+			<slot name="hidden-content">
+				<h1>Hidden Content of Row</h1>
+			</slot>
+		</div>
+	{/if}
+</button>
