15f7f4f64f3b66298355b2dc2d58fdbecea81767
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-20 20:38:59 +0600

    Update modal types

--- src/lib/types/modal.ts
+++ src/lib/types/modal.ts
@@ -1,4 +1,19 @@
+export interface ModalFooterButtonType {
+	onClick: () => void;
+	text: string;
+	classes?: string;
+    close: boolean;
+}
+
+export interface ModalFooterDataType {
+	note?: string;
+	buttons?: ModalFooterButtonType[];
+}
+
 export interface ModalStoreDataType {
+	show: boolean;
 	component: ConstructorOfATypedSvelteComponent;
+	title?: string;
+	footer?: ModalFooterDataType;
 	params?: Record<string, any>;
 }
