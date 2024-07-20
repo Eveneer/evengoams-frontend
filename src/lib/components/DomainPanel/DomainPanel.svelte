8c9574931c0612675e7d288a0e576dbfa1c671ad
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-20 20:39:58 +0600

    Update domain panel component based on new modal type

--- src/lib/components/DomainPanel/DomainPanel.svelte
+++ src/lib/components/DomainPanel/DomainPanel.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import AddIcon from '$lib/assets/icons/AddIcon.svelte';
 	import { modalStore } from '$lib/stores';
+	import type { ModalFooterButtonType } from '$lib/types/modal';
 	import Pagination from '../Pagination/Pagination.svelte';
 
 	export let paginationData;
 	export let modalComponent: ConstructorOfATypedSvelteComponent;
+	export let modalTitle: string | undefined = undefined;
+	export let modalButtons: ModalFooterButtonType[] | undefined = undefined;
+	export let modalFooterNote: string | undefined = undefined;
 
 	const setModalComponent = () => {
 		modalStore.set({
-			component: modalComponent
+			show: true,
+			component: modalComponent,
+			title: modalTitle,
+			footer: {
+				buttons: modalButtons,
+				note: modalFooterNote
+			}
 		});
 	};
 </script>
@@ -19,7 +29,7 @@
 		<div class="mx-auto xl:mx-0 my-auto">
 			<Pagination {paginationData} />
 		</div>
-		<button class="btn" type="button" on:click={setModalComponent}>
+		<button class="btn btn-primary" type="button" on:click={setModalComponent}>
 			<AddIcon size={25} />
 		</button>
 	</div>
