d9f482f8cacfb9fb99ec3c034a97d506738c20f7
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-20 20:40:32 +0600

    Update side nav button style

--- src/lib/components/Nav/SideNav/SideNavItem.svelte
+++ src/lib/components/Nav/SideNav/SideNavItem.svelte
@@ -5,19 +5,21 @@
 
 	export let item: NavItemType;
 	export let isActive: boolean = false;
+
+	$: navItemClasses = isActive
+		? 'bg-blue-500 text-white rounded-[5rem]'
+		: 'card border-transparent hover:bg-teal-300';
 </script>
 
 <a
 	href={item.type === 'link' ? item.href : '/#'}
-	class=" {isActive
-		? 'delay-300 bg-blue-500 text-white'
-		: 'card border-transparent hover:bg-teal-300'} rounded-2xl px-5 !py-3 flex flex-row justify-between group transit min-h-[58px]"
+	class="{navItemClasses} rounded-2xl !pl-3 !pr-0 !py-2.5 flex flex-row justify-between group min-h-[50px] shadow-xl"
 	use:autoAnimate
 >
 	<span class="my-auto">{item.label}</span>
 	{#if !isActive}
 		<ChevronIcon
-			classes="my-auto {isActive ? 'mr-0' : 'mr-5 group-hover:mr-0 '}"
+			classes="my-auto {isActive ? 'mr-0' : 'mr-3 group-hover:mr-0 '}"
 			size={30}
 			direction="right"
 		/>
