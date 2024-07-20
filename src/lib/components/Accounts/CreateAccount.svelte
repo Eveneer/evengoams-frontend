bfb4d0c616242ef37d98a25ed25d46424370c209
Mobashir Monim <mobashirmonim@gmail.com>
2024-07-20 21:54:29 +0600

    Update create account component and modal

--- src/lib/components/Accounts/CreateAccount.svelte
+++ src/lib/components/Accounts/CreateAccount.svelte
@@ -4,14 +4,10 @@
 	import RadioInput from '../Inputs/RadioInput.svelte';
 </script>
 
-<div class="relative bg-slate-300 rounded-2xl min-w-[500px]">
-	<div class="card flex flex-col gap-5 sticky top-[20px]">
-		<h3 class="text-center w-full p-2 bg-white rounded-full">Add Account</h3>
+<div class="flex flex-col gap-5">
+	<LineInput id="account_name" label="Account Name" callback={(s) => {}} />
 
-		<LineInput id="account_name" label="Account Name" callback={(s) => {}} />
+	<NumberInput id="account_balance" label="Initial Balance" callback={(n) => {}} />
 
-		<NumberInput id="account_balance" label="Initial Balance" callback={(n) => {}} />
-
-		<RadioInput label="Account Type" callback={(s) => {}} options={['Cash', 'Bank', 'Mobile']} />
-	</div>
+	<RadioInput label="Account Type" callback={(s) => {}} options={['Cash', 'Bank', 'Mobile']} />
 </div>
--- src/routes/accounts/+page.svelte
+++ src/routes/accounts/+page.svelte
@@ -39,7 +39,19 @@
 
 	<div class="w-[40%] relative">
 		<div class="flex flex-col gap-5 xl:gap-10 sticky top-[116px] xl:top-[136px]">
-			<DomainPanel {paginationData} modalComponent={CreateAccount} />
+			<DomainPanel
+				{paginationData}
+				modalComponent={CreateAccount}
+				modalTitle="Add Account"
+				modalButtons={[
+					{
+						text: 'Add Account',
+						onClick: () => {},
+						classes: 'btn-primary',
+						close: true
+					}
+				]}
+			/>
 
 			<div class="flex flex-col gap- h-[40vh] card w-full justify-center">
 				<h3 class="text-center">Stats Section</h3>
